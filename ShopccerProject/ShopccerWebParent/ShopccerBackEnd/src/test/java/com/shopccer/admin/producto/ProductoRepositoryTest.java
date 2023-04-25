cd62b3a47e3335cce0f131da54d41d803af03c4f
García Sánchez <antonio.garcia-sanchez@capgemini.com>
2023-04-25 10:44:02 +0200

    Creados test unitarios para repositorio de producto

--- ShopccerProject/ShopccerWebParent/ShopccerBackEnd/src/test/java/com/shopccer/admin/producto/ProductoRepositoryTest.java
+++ ShopccerProject/ShopccerWebParent/ShopccerBackEnd/src/test/java/com/shopccer/admin/producto/ProductoRepositoryTest.java
@@ -15,6 +15,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,7 +34,7 @@ public class ProductoRepositoryTest {
 
     @Test
     @Order(1)
-    public void testCreateProduct(){
+    public void testCreateProductAdidasKaiser(){
 
         Marca adidas = entityManager.find(Marca.class,1);
         Superficie naturalSeco = entityManager.find(Superficie.class,2);
@@ -52,5 +54,78 @@ public class ProductoRepositoryTest {
         Producto savedProducto = productoRepository.save(producto);
 
         assertThat(producto.getIdProducto()).isPositive();
+    }
+
+    @Test
+    @Order(2)
+    public void testCreateProductMizunoMorelia(){
+
+        Marca mizuno = entityManager.find(Marca.class,5);
+        Superficie naturalSeco = entityManager.find(Superficie.class,2);
+
+        Producto producto= new Producto();
+        producto.setNombre("Mizuno Morelia II Pro");
+        producto.setDescripcion("Perfecciona tu técnica con una bota que ofrece la sensación de ir descalzo gracias" +
+                " a su diseño con numerosas mejoras. Esta bota de fútbol de piel de canguro auténtica consigue que " +
+                "tengas la sensación de ir descalzo combinada con una gran durabilidad, que se mantendrá en el tiempo" +
+                " mientras tú alcanzas velocidades que no creías posibles.");
+        producto.setMarca(mizuno);
+        producto.setSuperficie(naturalSeco);
+        producto.setPrecio(109.99);
+        producto.setCratedTime(new Date());
+        producto.setUpdatedTime(new Date());
+
+        Producto savedProducto = productoRepository.save(producto);
+
+        assertThat(producto.getIdProducto()).isPositive();
+    }
+
+    @Test
+    @Order(3)
+    public void testListAllProductos() {
+
+        List<Producto> listProductos = (List<Producto>) productoRepository.findAll();
+
+        listProductos.forEach(p -> System.out.println(p.getNombre() +
+                "[ -- ]"+ p.getDescripcion()));
+
+        assertThat(listProductos).hasSize(2);
+
+    }
+
+    @Test
+    @Order(4)
+    public void testGetProductoById(){
+        Producto producto = productoRepository.findById(1).get();
+
+        assertThat("Adidas Kaiser 5 Liga").isEqualTo(producto.getNombre());
+
+    }
+
+    @Test
+    @Order(5)
+    public void testUpdateProducto(){
+        Producto producto = productoRepository.findById(1).get();
+
+        producto.setPrecio(65.79);
+        producto.setUpdatedTime(new Date());
+
+        productoRepository.save(producto);
+
+        System.out.println(productoRepository.findById(1).get().getPrecio());
+
+        assertThat(65.79).isEqualTo(productoRepository.findById(1).get().getPrecio());
+
+
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteProducto(){
+
+        productoRepository.deleteById(2);
+        Optional<Producto> producto = productoRepository.findById(2);
+
+        assertThat(!producto.isPresent());
     }
 }
