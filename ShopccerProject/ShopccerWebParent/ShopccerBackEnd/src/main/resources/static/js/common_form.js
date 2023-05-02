c6dd260b591da0384546eee231e8b084b3fc5474
Antonio Garcia <ags0389@gmail.com>
2023-05-03 00:14:13 +0200

    Añadiendo imagenes detalle, sin acabar

--- ShopccerProject/ShopccerWebParent/ShopccerBackEnd/src/main/resources/static/js/common_form.js
+++ ShopccerProject/ShopccerWebParent/ShopccerBackEnd/src/main/resources/static/js/common_form.js
@@ -1,22 +1,16 @@
-$(document)
-	.ready(
-		function() {
-			/**$('input:radio[name="rol"]').filter('[value="2"]').attr('checked', true);*/
-			$("#fileImg")
-				.change(
-					function() {
-						fileSize = this.files[0].size;
+$(document).ready(	function() {
+	$("#fileImg").change(function() {
+		fileSize = this.files[0].size;
 
-						if (fileSize > 1048576) {//1MB
-							this
-								.setCustomValidity("Debes seleccionar una imagen de una tamaño menor a 1MB");
-							this.reportValidity();
-						} else {
-							showImageThumbnail(this);
-						}
+		if (fileSize > 1048576) {//1MB
+			this.setCustomValidity("Debes seleccionar una imagen de una tamaño menor a 1MB");
+			this.reportValidity();
+		} else {
+			showImageThumbnail(this);
+		}
 
-					})
-		});
+	})
+});
 
 function showImageThumbnail(fileInput) {
 	var file = fileInput.files[0];
