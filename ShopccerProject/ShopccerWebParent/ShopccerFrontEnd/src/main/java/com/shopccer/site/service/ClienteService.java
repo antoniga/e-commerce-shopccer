1068335371515331480e923aff4527b27d00c35c
Antonio Garcia <ags0389@gmail.com>
2023-05-16 12:41:43 +0200

    Trabajando en el registro de clientes

--- ShopccerProject/ShopccerWebParent/ShopccerFrontEnd/src/main/java/com/shopccer/site/service/ClienteService.java
+++ ShopccerProject/ShopccerWebParent/ShopccerFrontEnd/src/main/java/com/shopccer/site/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.shopccer.site.service;
 
+import com.shopccer.common.entity.Cliente;
 import com.shopccer.common.entity.Pais;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +12,6 @@ public interface ClienteService {
     List<Pais> listAllPaises();
 
     boolean isEmailUnique(String email);
+
+    void registerCustomer(Cliente cliente);
 }
--- ShopccerProject/ShopccerWebParent/ShopccerFrontEnd/src/main/java/com/shopccer/site/service/impl/ClienteServiceImpl.java
+++ ShopccerProject/ShopccerWebParent/ShopccerFrontEnd/src/main/java/com/shopccer/site/service/impl/ClienteServiceImpl.java
@@ -6,17 +6,25 @@ import com.shopccer.site.repository.ClienteRepository;
 import com.shopccer.site.repository.PaisRepository;
 import com.shopccer.site.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
+
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
     @Autowired
-    ClienteRepository clienteRepository;
+    private ClienteRepository clienteRepository;
 
     @Autowired
-    PaisRepository paisRepository;
+    private PaisRepository paisRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public List<Pais> listAllPaises() {
@@ -27,5 +35,41 @@ public class ClienteServiceImpl implements ClienteService {
     public boolean isEmailUnique(String email) {
         Cliente cliente = clienteRepository.findByEmail(email);
         return cliente == null;
+    }
+    @Override
+    public void registerCustomer(Cliente cliente) {
+        encodePassword(cliente);
+        cliente.setActivo(false);
+        cliente.setCreatedTime(new Date());
+
+
+        String randomCode = generateRandomCode(64);
+        cliente.setCodigoVerificacion(randomCode);
+
+        clienteRepository.save(cliente);
+
+    }
+
+    private void encodePassword(Cliente cliente) {
+        String encodedPassword = passwordEncoder.encode(cliente.getPassword());
+        cliente.setPassword(encodedPassword);
+    }
+
+    private String generateRandomCode(Integer longitud){
+
+        Integer length = longitud; // Longitud del string aleatorio
+        String caracteresValidos = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            Integer index = random.nextInt(caracteresValidos.length());
+            char randomChar = caracteresValidos.charAt(index);
+            sb.append(randomChar);
+        }
+
+        String randomCode = sb.toString();
+
+        return randomCode;
     }
 }
