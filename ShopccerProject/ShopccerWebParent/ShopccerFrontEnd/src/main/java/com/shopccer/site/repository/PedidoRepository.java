9ddb0703f1dc0d7219d8e82a017cd8d0ca9d5dcd
Antonio Garcia <ags0389@gmail.com>
2023-06-02 17:09:18 +0200

    Desarrollada funcionalidad para que cada cliente pueda listarlos pedidos

--- ShopccerProject/ShopccerWebParent/ShopccerFrontEnd/src/main/java/com/shopccer/site/repository/PedidoRepository.java
+++ ShopccerProject/ShopccerWebParent/ShopccerFrontEnd/src/main/java/com/shopccer/site/repository/PedidoRepository.java
@@ -10,8 +10,9 @@ public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
     @Query("SELECT DISTINCT p FROM Pedido p JOIN p.detallePedido pd JOIN pd.producto pr "
             + "WHERE p.cliente.idCliente = ?2 "
-            + "AND (pr.nombre LIKE %?1% OR p.estado LIKE %?1%)")
+            + "AND (pr.nombre LIKE %?1%)")
     Page<Pedido> findAll(String palabraClave, Integer idCliente, Pageable pageable);
+
 
     @Query("SELECT p FROM Pedido p WHERE p.cliente.idCliente = ?1")
     Page<Pedido> findAll(Integer idCliente, Pageable pageable);
